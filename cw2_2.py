ef97274c4dc13da34e6751f2f134ef93c8002ee2
Jonathan Meyer <meyer.jonathan@free.fr>
2017-11-21 15:41:36 +0000

    V0.40 	Working : 		Fixed / improve main file loading 		Added also likes functionnality
    	To do :
    		Align original document & original reader on also_likes graph

--- cw2_2.py
+++ cw2_2.py
@@ -14,16 +14,22 @@ import tkinter as tk
 from tkinter import filedialog, StringVar
 from tkinter import ttk
 
-from itertools import takewhile, repeat
+from itertools import takewhile, repeat, chain
 import threading, time
 import psutil, os, sys, random
 from subprocess import run, PIPE
 from tqdm import tqdm, tqdm_pandas
 import contextlib
+import collections
+import heapq
+import operator
 
 style.use('ggplot')
 plt.ion()
 CHUNK_SIZE = 5000
+
+
+######### USE READ EVENTS
 
 
 plots = {"None":"None",
@@ -86,7 +92,36 @@ class Gui:
         self.pg2_val = StringVar(self.root)
         self.progressbar2 = ttk.Progressbar(self.root, mode='determinate', variable=self.pg2_val)
         self.progressbar2.grid(column=2, row=5, columnspan=2, sticky='nsew')
+        
+        button2 = ttk.Button(self.root, text="Get document readers", command=lambda : self._also_likes_rnd())#('232eeca785873d35')) 
+        button2.grid(column=3, row=3)
 
+    def freq_ascending_sort(self, data): # Data as : [('<DOC_ID>', <count>), ('<DOC_ID>', <count>)...], returns ['<DOC_ID>', '<DOC_ID>'...]
+        return data.reverse() # Data sorted (max -> min) by Counter
+    
+    def _also_likes_rnd(self):
+        rnd = random.randrange(0,len(self.model.data))
+        self.model.also_likes(self.model.data.iloc[rnd]['subject_doc_id'], self.freq_descending_sort, self.model.data.iloc[rnd]['visitor_uuid'])
+        
+    def freq_descending_sort(self, data):
+        return data # Data already sorted (max -> min) by Counter
+    
+    def biased_sort(self, data): # Data : JSON encoded strings {"doc":"<DOC_ID>", "weight":"<weight>"} (1 per line)
+        doc_weights = {}
+        with open("doc_weights.csv") as f:
+            for line in f.readlines():
+                (doc_id, weight) = line.split(',')
+                doc_weights[doc_id] = int(weight)
+                            
+        sorted_docs = []
+        
+        for doc_id, freq in data:
+            sorted_docs.append((doc_id, doc_weights.get(doc_id, freq))) # If we have no bias toward this document, simply use its frequency
+        
+        sorted_docs =  sorted(sorted_docs, key=lambda x:x[1], reverse=True)
+        
+        return sorted_docs
+            
     def file_loaded_cb(self):
         self.ready_lb.config(bg="green")
         self.dropdown.config(state="normal")
@@ -119,8 +154,11 @@ class Gui:
         if var != "None" and self.model.add_column(var):
             print(f"ploting {var}")
             
-            df = self.model.data.loc[self.model.data['subject_doc_id'] == random.choice(self.model.data['subject_doc_id'])]#'130705172251-3a2a725b2bbd5aa3f2af810acf0aeabb']
-           
+            df = self.model.data 
+            dfmi.loc[:,('one','second')]
+            df.loc[df['subject_doc_id'] == df.iloc[random.randrange(0,len(df))]['subject_doc_id']] #'130705172251-3a2a725b2bbd5aa3f2af810acf0aeabb'] '130705172251-3a2a725b2bbd5aa3f2af810acf0aeabb']
+            df.drop_duplicates(['visitor_uuid'], inplace=True)
+            
             # Get sum on the given column and sort in descending order
             df = df.groupby([var], as_index=False).size().reset_index(name='counts')
             df = df.sort_values('counts', ascending=False)
@@ -165,12 +203,14 @@ class Model:
         i = 1
         tmp = []
         for df in pd.read_json(filepath, lines=True, chunksize=CHUNK_SIZE): 
+            df = df.loc[df['event_type'] == 'read']
             tmp.append(df[['visitor_uuid','visitor_country', 'visitor_useragent', 'subject_doc_id']])  
             pg_val.set(i)
             i +=1
         
         self.data = pd.concat(tmp, axis=0)
-
+        self.data.drop_duplicates(['visitor_uuid', 'subject_doc_id'], inplace=True) # Also do it on each chunk to reduce each chunk size and speed up concatenation ? Mmm..
+        
         self.done_loading.set(True)
         print("Loading done")
         df_mem_usage = self.data.memory_usage().sum()/(1024*1024)
@@ -212,48 +252,193 @@ class Model:
          
         print(f"Done preprocessing - {time.time()- start_time} sec")
         
-        
-        
+            
     def _preprocess_browser(self, df):
         df['visitor_browser'] = df['visitor_useragent'].apply(lambda x: ua.parse(x).browser.family)      
         
     def _preprocess_platform(self, df):
         df['visitor_platform'] = df['visitor_useragent'].apply(lambda x: 'Mobile' if ua.parse(x).is_mobile else 'Desktop')        
             
-            
-            
-    """def preprocess_browser(self):
-        start_time = time.time() 
-        self.gui.progressbar2["maximum"] = 100
-        df_len = len(self.data.index)
-        split = np.array_split(self.data, 100) # Data is not copied, no ram increase !
-
-        self.data['visitor_browser'] = np.nan
-        for df, i in zip(split, range(1,101)):
-            df['visitor_browser'] = df['visitor_useragent'].apply(lambda x: ua.parse(x).browser.family)
-            self.gui.pg2_val.set(i)
-        
-        self.data = pd.concat(split)
-         
-        print(f"Done preprocessing - {time.time()- start_time} sec")
-        
+    def get_document_readers(self, doc_id):
+        data = self.data.loc[self.data['subject_doc_id'] == doc_id]['visitor_uuid'].unique()
+        return data.tolist()
     
-    def preprocess_platform(self):
-        start_time = time.time() 
-        self.gui.progressbar2["maximum"] = 100
-        df_len = len(self.data.index)
-        split = np.array_split(self.data, 100) # Data is not copied, no ram increase !
+    def visitor_views(self, vis_id):
+        data = self.data.loc[self.data['visitor_uuid'] == vis_id]['subject_doc_id'].unique() # /!\ np.array not pandas.Series
+        data = data[~pd.isnull(data)] # Remove nan values, data[True/False with '~' = pandas series inverse]
+        return data
+     
+    def also_likes(self, doc_id, sort, ori_reader = None):
+        start = time.time()
+        docs = []
+        dic = {}
+        #import pdb; pdb.set_trace()
+        print(f"Original reader : {ori_reader}, original doc : {doc_id}")
+        also_readers = self.get_document_readers(doc_id)
+        print(f"readers of the same doc : {also_readers}")
+       
+        if ori_reader:
+            also_readers.remove(ori_reader)  
+                    
+        for reader in self.get_document_readers(doc_id):
+            #if reader != ori_reader:
+            for doc in self.visitor_views(reader):
+                if doc not in dic:
+                    dic[doc] = []
+                dic[doc].append(reader)
+           #docs.extend((reader, self.visitor_views(reader)))
+        
+        #import pdb; pdb.set_trace()
+        #docs = collections.Counter(docs).most_common(10) # Optimized (tested vs dictionnary + sort + indexing and dictionnary + heapq.nlargest) 
+        #docs = sort(docs)
+        #docs = [x[0] for x in docs]
+        dot_graph = GraphBuilder().build_tree(doc_id, dic, ori_reader)
+        #print(dot_graph)
+        with open("my_diag.dot", "w") as f:
+            f.write(dot_graph)
+        
+        print(f"Time : {time.time() - start} sec")
+        
+        print (dic)
+        #import pdb; pdb.set_trace()
+        return dic
 
-        self.data['visitor_platform'] = np.nan
-        for df, i in zip(split, range(1,101)):
-            df['visitor_platform'] = df['visitor_useragent'].apply(lambda x: 'Mobile' if ua.parse(x).is_mobile else 'Desktop')        
-            self.gui.pg2_val.set(i)
+class GraphBuilder:
+    default_graph_header = ["digraph also_likes {",
+                            "ranksep=.75; ratio=compress; size = \"15,22\"; orientation=landscape; rotate=180;",
+                            "{",
+                            "node [shape=plaintext, fontsize=16];",
+                            "Readers -> Documents "]
+                        
+    def __init__(self, graph_header = None):
+        if graph_header == None:
+            graph_header = GraphBuilder.default_graph_header
+        self.graph = Graph(graph_header, ori_design="style=filled,color=\".3 .9 .7\"")
+       
+    def build_tree(self, ori_doc, dic, ori_reader=None):
+ 
+        ori_doc = ori_doc[-4:]
+        if ori_reader:
+            ori_reader = ori_reader[-4:]
+    
+        n = {}
+        for d, readers in dic.items():
+            n[d[-4:]] = []
+            for reader in readers:
+                n[d[-4:]].append(reader[-4:])
+                
+        #dic = {d[-4:]: v[-4:] for d, v in dic.items()}
         
-        self.data = pd.concat(split)
-         
-        print(f"Done preprocessing - {time.time()- start_time} sec")"""
+        self.add_ori(ori_doc, ori_reader)
+        self.add_other_docs(set(n.keys()))
+        self.add_other_readers(set(chain.from_iterable(n.values())))
         
+        if ori_reader:
+            self.add_link(ori_reader, ori_doc)
         
+        #import pdb; pdb.set_trace()
+        for doc, readers in n.items():
+            for reader in readers:
+                self.add_link(reader, doc)
+                
+        for reader in chain.from_iterable(n.values()):
+            self.add_link(reader, ori_doc)
+
+        return self.graph.get_graph()
+        
+    def add_other_docs(self, doc_lst):
+        for doc in doc_lst:
+            self.graph.add_document(doc)
+     
+    def add_other_readers(self, readers_lst):
+        for reader in readers_lst:
+            self.graph.add_reader(reader)
+            
+    def add_ori(self, ori_doc, ori_reader):
+          self.graph.add_document(ori_doc, ori=True)
+          if ori_reader:
+            self.graph.add_reader(ori_reader, ori=True)
+
+    def add_link(self, reader, doc):
+        self.graph.add_link(reader, doc)
+        
+# TODO : Make generic sections
+class Graph:
+    def __init__(self, header, ori_design):
+        self.header = header
+        self.ori_design = ori_design
+        self.labels = ["[label=\"Size: 1m\"];"]
+        self.readers = ["{ rank = same; \"Readers\";"]
+        self.docs = ["{ rank = same; \"Documents\";"]
+        self.links = []
+    
+    def add_document(self, doc, align_key=-1, ori=False):
+        lb = f"\"{doc}\" [label=\"{doc}\", shape=\"circle\""
+        if ori:
+            lb += self.ori_design
+        
+        lb += "];"
+        if f"\"{doc}\";" not in self.docs:
+            self.docs.append(f"\"{doc}\";")
+            self.labels.append(lb)
+        
+    def add_reader(self, reader, align_key=-1, ori=False):
+        lb = f"\"{reader}\" [label=\"{reader}\", shape=\"box\""
+        if ori:
+            lb += self.ori_design
+        
+        lb += "];"
+        if f"\"{reader}\";" not in self.readers:
+            self.readers.append(f"\"{reader}\";")
+            self.labels.append(lb)
+        
+    def add_link(self, reader, doc):
+        if f"\"{reader}\" -> \"{doc}\";" not in self.links:
+            self.links.append(f"\"{reader}\" -> \"{doc}\";")
+    
+    """def rearrange_order(self):
+        # Overly complicated way to find the original reader and document :)
+        tmp = list(filter(lambda x : "shape=\"box\",style=filled" in x, self.labels))
+        if len(tmp):
+            ori_reader = tmp[0]
+            
+        tmp = list(filter(lambda x : "shape=\"circle\",style=filled" in x, self.labels))
+        if len(tmp):
+            ori_doc = tmp[0]
+            
+        ori_reader_pos = int(len(self.readers) / 2)
+        ori_doc_pos = int(len(self.docs) / 2)
+        
+        new_docs = []
+        self.docs.remove(ori_doc)
+        
+        for idx, doc in enumerate(self.docs):
+            if idx == ori_doc_pos:
+                new_docs.append(ori_doc)
+            new_docs.append(doc)
+        
+        new_readers = []
+        self.readers.remove(ori_reader)
+        
+        for idx, reader in enumerate(self.readers):
+            if idx == ori_reader_pos:
+                new_readers.append(ori_reader)
+            new_docs.append(reader)
+    
+        self.docs = new_docs
+        self.readers = new_readers"""
+        
+    def get_graph(self):
+        #self.rearrange_order()
+        self.readers.append("};")
+        self.docs.append("};")
+        self.links.append("};")
+        graph = self.header + self.labels + self.readers + self.docs + self.links
+        graph.append("}")
+        graph = '\n'.join(graph)
+        
+        return graph
+    
 class DataAnalyser:
     def __init__(self):
         root = tk.Tk()
